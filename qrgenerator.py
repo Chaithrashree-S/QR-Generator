4853ce2f3ee5fe9c4e57ca0e59c2725d4276bcd3
Chaithrashree S <96301006+Chaithrashree-S@users.noreply.github.com>
2024-02-28 12:31:33 +0530

    Add files via upload

---
+++ qrgenerator.py
@@ -0,0 +1,19 @@
+import qrcode
+def generate_qr_code(url, filename):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(filename)
+
+if __name__ == "__main__":
+    url = input("Enter the URL to generate QR code: ")
+    filename = input("Enter the filename to save the QR code image (with extension): ")
+    generate_qr_code(url, filename)
+    print("QR code generated successfully!")
